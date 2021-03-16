2238e7ffe3b155ba4aeb06d7231cc91aaf238af8
Lnkyoung16 <magical3684@gmail.com>
2021-03-16 21:34:40 +0900

    third commit

--- number_guessing_game.py
+++ number_guessing_game.py
@@ -4,9 +4,14 @@ from cipher_check_3 import cipher_check_3
 
 random_number = []
 
-for n in range(0,3):
-    random_number.append(random.randint(0,9))
-    123
+for k in range(0, 3):
+    if k == 0:
+        random_number.append(random.randint(1, 9))
+    else:
+        random_number.append(random.randint(0, 9))
+
+print(random_number)
+
 s = 0
 a = 1
 while s < 3:
@@ -16,19 +21,24 @@ while s < 3:
     e = 0
     number_input = int(input("숫자를 입력하세요:"))
     input_result = cipher_check_3(number_input)
+
     for d in range(0, 3):
-        for e in range(0, 3):
+        for e in range(0, len(input_result)):
             if random_number[d] == input_result[e]:
                 if d == e:
                     s = s + 1
-                else:
-                    n = n + 1
-    if s > 0 and n > 0:
-        print(s, "개의 숫자가 자리와 숫자가 일치합니다. ", n, "개의 숫자가 숫자만 일차합니다.")
+    z = 0
+    for x in range(0,10):
+        y = input_result.count(x)
+        if y > 1:
+            z = y
+
+    if s > 0 or n > 0:
+        if s == 3:
+            print("정답입니다! 숫자는 ", random_number[0], random_number[1], random_number[2], "입니다. ", a, "번의 시도를 통하여 맞추셨습니다.")
+            break
+        else:
+            print(s, "개의 숫자가 자리와 숫자가 일치합니다. ", n, "개의 숫자가 숫자만 일치합니다.")
     else:
         print("모든 숫자가 일치하지 않습니다.")
     a = a + 1
-    if s == 3:
-        print("정답입니다! 숫자는 ", random_number[0], random_number[1], random_number[2], "입니다. ", a, "번의 시도를 통하여 맞추셨습니다.")
-        break
-
